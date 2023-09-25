06565b95aabdffc77fa5aba4e796b3d082938dcd
Franz Thiemann <franz@thiemann.io>
2023-09-25 11:26:11 +0200

    Implemented an option to shift the unit cell when reading output from AMS so that the unit cell and the longbond mechanism work. Compatibility with the density plottig is achived by shifting the density by the same amount

--- new_importASE/__init__.py
+++ new_importASE/__init__.py
@@ -20,27 +20,29 @@ import bpy
 from bpy_extras.io_utils import ImportHelper
 from ase import io
 import ase
-from ase.data import covalent_radii,colors
+from ase.data import covalent_radii, colors
 from ase.build import make_supercell
 import numpy as np
 from ase import Atoms
 from os.path import join
 import os
 from .import_cubefiles import cube2vol
-from .utils import setup_materials,group_atoms
-from .drawobjects import draw_atoms,draw_bonds,draw_unit_cell
+from .utils import setup_materials, group_atoms
+from .drawobjects import draw_atoms, draw_bonds, draw_unit_cell
 from .ui import import_ase_molecule
+
+
 class ImportASEMolecule(bpy.types.Operator, ImportHelper):
     bl_idname = "import_mesh.ase"
     bl_label = "Import ASE Molecule"
     bl_options = {"REGISTER", "UNDO"}
 
     filename_ext = ".*"
-   
+
     supercell1: bpy.props.IntVectorProperty(
         name="Supercell-vector",
         size=9,
-        default=[1, 0, 0, 0,1,0, 0,0,1],
+        default=[1, 0, 0, 0, 1, 0, 0, 0, 1],
     )
     scale: bpy.props.FloatProperty(
         name="Scale",
@@ -53,7 +55,7 @@ class ImportASEMolecule(bpy.types.Operator, ImportHelper):
         name='colorbonds',
         description="Color the bonds according to the surrounding atoms",
         default=False,
-            )
+    )
     fixbonds: bpy.props.BoolProperty(
         name='Use Longbonds',
         description="Mitigates lines in the middle of bonds where individual bonds meet",
@@ -70,19 +72,19 @@ class ImportASEMolecule(bpy.types.Operator, ImportHelper):
         name='unit_cell',
         description="Draw unit cell",
         default=False,
-            )
+    )
     separate_collections: bpy.props.BoolProperty(
         name='separate_collections',
         description="separate collections by unit cell, atoms and bonds",
         default=False,
-            )
+    )
     representation: bpy.props.EnumProperty(
         name="representation",
         description="select the representation for your structure",
         items=[
-            ("Balls'n'Sticks","Balls'n'Sticks", "Balls and sticks representaiton"),
-            ("Licorice","Licorice", "Licorice representation"),
-            ('VDW','VDW','VDW Radii, no bonds'),
+            ("Balls'n'Sticks", "Balls'n'Sticks", "Balls and sticks representaiton"),
+            ("Licorice", "Licorice", "Licorice representation"),
+            ('VDW', 'VDW', 'VDW Radii, no bonds'),
         ],
         default="Balls'n'Sticks"
     )
@@ -90,20 +92,27 @@ class ImportASEMolecule(bpy.types.Operator, ImportHelper):
         name='load e-density',
         description="load electron-density as volume and use a node-tree for the creation of isosurfaces (only .cube-files)",
         default=True,
-            )
+    )
+    zero_cell: bpy.props.BoolProperty(
+        name='Assume zero centered cell',
+        description="Calculations in AMS result in zero-centered cells. This will confuse the longbond algorithm and cause" \
+                    "the atoms to lie outside the unit cell (if drawn). This option compensates for that.",
+        default=False,
+    )
     files: bpy.props.CollectionProperty(
         type=bpy.types.OperatorFileListElement,
-        options={'HIDDEN','SKIP_SAVE'},
+        options={'HIDDEN', 'SKIP_SAVE'},
         description='List of files to be imported'
     )
-    directory:bpy.props.StringProperty(
+    directory: bpy.props.StringProperty(
         name='folder',
         description='directory of file',
         subtype='DIR_PATH'
     )
+
     def draw(self, context):
         layout = self.layout
-        box= layout.box()
+        box = layout.box()
         box.label(text="")
         box.separator()
         box.operator("import_scene.my_format", text="Import")
@@ -111,44 +120,51 @@ class ImportASEMolecule(bpy.types.Operator, ImportHelper):
             row = box.row(align=True)
             for j in range(3):
                 row.prop(self, "supercell1", index=i * 3 + j, emboss=False, slider=True)
-        layout.prop(self,"scale")
-        layout.prop(self,'colorbonds')
+        layout.prop(self, "scale")
+        layout.prop(self, 'colorbonds')
         layout.prop(self, 'fixbonds')
-        layout.prop(self,'representation')
-        layout.prop(self,'color')
-        layout.prop(self,'unit_cell')
-        layout.prop(self,'separate_collections')
-        layout.prop(self,'read_density')
+        layout.prop(self, 'representation')
+        layout.prop(self, 'color')
+        layout.prop(self, 'unit_cell')
+        layout.prop(self, 'separate_collections')
+        layout.prop(self, 'read_density')
+        layout.prop(self, 'zero_cell')
+
     def execute(self, context):
         for file in self.files:
             filepath = join(self.directory, file.name)
             matrix = np.array(self.supercell1).reshape((3, 3))
-            default=[1, 0, 0, 0,1,0, 0,0,1]
-            SUPERCELL=False
-            for n,i in enumerate(self.supercell1):
+            default = [1, 0, 0, 0, 1, 0, 0, 0, 1]
+            SUPERCELL = False
+            for n, i in enumerate(self.supercell1):
                 if i != default[n]:
-                    SUPERCELL=True
+                    SUPERCELL = True
                 break
             import_ase_molecule(filepath, file.name, matrix,
                                 color=self.color, colorbonds=self.colorbonds, fixbonds=self.fixbonds, scale=self.scale,
                                 unit_cell=self.unit_cell, representation=self.representation,
                                 separate_collections=self.separate_collections,
-                                read_density=self.read_density, SUPERCELL=SUPERCELL)
+                                read_density=self.read_density, SUPERCELL=SUPERCELL, shift_cell=self.zero_cell)
         return {"FINISHED"}
+
     def invoke(self, context, event):
-        context.window_manager.fileselect_add(self) 
-        return {'RUNNING_MODAL'} 
+        context.window_manager.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
 
 def menu_func_import(self, context):
     self.layout.operator(ImportASEMolecule.bl_idname, text="ASE Molecule (.*)")
+
 
 def register():
     bpy.utils.register_class(ImportASEMolecule)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
+
 def unregister():
     bpy.utils.unregister_class(ImportASEMolecule)
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+
 
 if __name__ == "__main__":
     register()
--- new_importASE/ui.py
+++ new_importASE/ui.py
@@ -16,13 +16,19 @@ from .drawobjects import draw_atoms, draw_bonds, draw_unit_cell, draw_bonds_new
 def import_ase_molecule(filepath, filename, matrix, colorbonds=False, fixbonds=False, color=0.2, scale=1,
                         unit_cell=False,
                         representation="Balls'n'Sticks", separate_collections=False,
-                        read_density=True, SUPERCELL=True
+                        read_density=True, SUPERCELL=True, shift_cell=False
                         ):
     atoms = ase.io.read(filepath)
+    # When importing molecules from AMS, the resulting atoms do not lie in the unit cell since AMS uses unit cells centered around 0
+    shift_cell = True
+    cell = atoms.cell
+    shift_vector = 0.5 * cell[0] + 0.5 * cell[1] + 0.5 * cell[2]
+    if shift_cell:
+        atoms.positions += shift_vector
     #    if SUPERCELL == True:
     #        atoms=make_supercell(atoms,matrix)
     setup_materials(atoms, colorbonds=colorbonds, color=color)
-    if separate_collections == True:
+    if separate_collections:
         my_coll = bpy.data.collections.new(name=atoms.get_chemical_formula() + '_' + filename.split('.')[0] + '_atoms')
     else:
         my_coll = bpy.data.collections.new(name=atoms.get_chemical_formula() + '_' + filename.split('.')[0])
@@ -32,7 +38,7 @@ def import_ase_molecule(filepath, filename, matrix, colorbonds=False, fixbonds=F
     group_atoms(atoms)
     draw_atoms(atoms, scale=scale, representation=representation)
     if representation != 'VDW':
-        if separate_collections == True:
+        if separate_collections:
             my_coll = bpy.data.collections.new(
                 name=atoms.get_chemical_formula() + '_' + filename.split('.')[0] + '_bonds')
             bpy.context.scene.collection.children.link(my_coll)
@@ -43,13 +49,24 @@ def import_ase_molecule(filepath, filename, matrix, colorbonds=False, fixbonds=F
         else:
             draw_bonds(atoms)
     if unit_cell == True and atoms.pbc.all() != False:
-        if separate_collections == True:
+        if separate_collections:
             my_coll = bpy.data.collections.new(
                 name=atoms.get_chemical_formula() + '_' + filename.split('.')[0] + '_cell')
             bpy.context.scene.collection.children.link(my_coll)
             layer_collection = bpy.context.view_layer.layer_collection.children[my_coll.name]
             bpy.context.view_layer.active_layer_collection = layer_collection
         draw_unit_cell(atoms)
-    if read_density == True:
+    if read_density:
         if 'cube' in filename:
-            atoms = cube2vol(filepath)
+            density_obj = cube2vol(filepath)
+            print(density_obj)
+            density_obj.location.x += shift_vector[0]
+            density_obj.location.y += shift_vector[1]
+            density_obj.location.z += shift_vector[2]
+            # name = cube2vol(filepath)
+            # name = name.split('.')[0].split("/")[-1]
+            # bpy.data.objects[name].location.x += shift_vector[0]
+            # bpy.data.objects[name].location.y += shift_vector[1]
+            # bpy.data.objects[name].location.z += shift_vector[2]
+
+
