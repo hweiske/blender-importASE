dd7569c4e9728e1093258d502573cf5b8f9aa4a5
Patrick Melix <patrick.melix@uni-leipzig.de>
2024-08-16 10:17:22 +0200

    revert breaking change in init

--- blender_importASE/__init__.py
+++ blender_importASE/__init__.py
@@ -30,46 +30,46 @@ class ImportASEMolecule(bpy.types.Operator, ImportHelper):
 
     filename_ext = ".*"
 
-    supercell1 = bpy.props.IntVectorProperty(
+    supercell1: bpy.props.IntVectorProperty(
         name="Supercell-vector",
         size=9,
         default=[1, 0, 0, 0, 1, 0, 0, 0, 1],
     )
-    scale = bpy.props.FloatProperty(
+    scale: bpy.props.FloatProperty(
         name="Scale",
         description="scaling the atoms",
         default=1.0,
         min=0.0,
         soft_max=10,
     )
-    colorbonds = bpy.props.BoolProperty(
+    colorbonds: bpy.props.BoolProperty(
         name='colorbonds',
         description="Color the bonds according to the surrounding atoms",
         default=False,
     )
-    fix_bonds = bpy.props.BoolProperty(
+    fix_bonds: bpy.props.BoolProperty(
         name='Use Longbonds',
         description="Mitigates lines in the middle of bonds where individual bonds meet",
         default=True,
     )
-    color = bpy.props.FloatProperty(
+    color: bpy.props.FloatProperty(
         name="color",
         description="color for gray bonds in BW-scale",
         default=0.6,
         min=0.0,
         max=1.0,
     )
-    unit_cell = bpy.props.BoolProperty(
+    unit_cell: bpy.props.BoolProperty(
         name='unit_cell',
         description="Draw unit cell",
         default=False,
     )
-    separate_collections = bpy.props.BoolProperty(
+    separate_collections: bpy.props.BoolProperty(
         name='separate_collections',
         description="separate collections by unit cell, atoms and bonds",
         default=False,
     )
-    representation = bpy.props.EnumProperty(
+    representation: bpy.props.EnumProperty(
         name="representation",
         description="select the representation for your structure",
         items=[
@@ -79,33 +79,33 @@ class ImportASEMolecule(bpy.types.Operator, ImportHelper):
         ],
         default="Balls'n'Sticks"
     )
-    read_density = bpy.props.BoolProperty(
+    read_density: bpy.props.BoolProperty(
         name='load e-density',
         description="load electron-density as volume and use a node-tree for the creation of isosurfaces (only .cube-files)",
         default=True,
     )
-    zero_cell = bpy.props.BoolProperty(
+    zero_cell: bpy.props.BoolProperty(
         name='Assume zero centered cell',
         description="Calculations in AMS result in zero-centered cells. This will confuse the longbond algorithm and cause" \
                     "the atoms to lie outside the unit cell (if drawn). This option compensates for that.",
         default=False,
     )
-    animate = bpy.props.BoolProperty(
+    animate: bpy.props.BoolProperty(
         name='animate',
         description="animate trajectory (all traj-files readable by ASE)",
         default=True,
     )
-    imageslice = bpy.props.IntProperty(
+    imageslice: bpy.props.IntProperty(
         name='nth-image',
         description='when loading long trajectories it is recommended not to use all images, since that will scale poorly depending on the number of bonds in the molecule and drastically influence performance',
         default=1
     )
-    files = bpy.props.CollectionProperty(
+    files: bpy.props.CollectionProperty(
         type=bpy.types.OperatorFileListElement,
         options={'HIDDEN', 'SKIP_SAVE'},
         description='List of files to be imported'
     )
-    directory = bpy.props.StringProperty(
+    directory: bpy.props.StringProperty(
         name='folder',
         description='directory of file',
         subtype='DIR_PATH'
